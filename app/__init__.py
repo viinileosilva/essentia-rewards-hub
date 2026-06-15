ff5202fb38729a630140976dc1d7eb0244fb1925
Vinicius Silva <viinileosilva@hotmail.com>
2026-06-15 14:35:43 -0300

    Configurado para Render + PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from sqlalchemy.pool import StaticPool
 from config import Config
 
 db = SQLAlchemy()
@@ -14,20 +13,6 @@ login_manager.login_message = 'Faça login para continuar.'
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
-    if Config.USE_TURSO:
-        import libsql_experimental as libsql
-
-        def _turso_creator():
-            return libsql.connect(
-                database=Config.TURSO_DATABASE_URL,
-                auth_token=Config.TURSO_AUTH_TOKEN,
-            )
-
-        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-            'creator': _turso_creator,
-            'poolclass': StaticPool,
-        }
 
     db.init_app(app)
     migrate.init_app(app, db)
